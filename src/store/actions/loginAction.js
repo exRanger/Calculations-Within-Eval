959f88e6aaac51babf06d062a16112ff1db9f507
Ilgiz <lmnasxzz@gmail.com>
2021-12-09 23:38:42 +0300

    Delete src/store directory

--- src/store/actions/loginAction.js
+++
@@ -1,8 +0,0 @@
-import { ENABLE_LOGIN_BUTTON } from "../types/loginType"
-
-export const setIsLogin = (payload) => {
-    return {
-        type: ENABLE_LOGIN_BUTTON,
-        payload
-    }
-} 
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from 'redux'
-import {reducers} from './reducers/'
-
-
-
-export const store = createStore(reducers)
-
-
